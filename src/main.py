11ceee0c64f51c271b03af897da76b418f6866a6
WilliGerber <49000944+WilliGerber@users.noreply.github.com>
2023-10-15 18:47:19 -0300

    import theme and created add/edit pages

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from settings import HOST, PORT, DEBUG
 
 from mod_funcionario.funcionario import bp_funcionario
@@ -7,6 +7,14 @@ from mod_produto.produto import bp_produto
 from mod_index.index import bp_index
 
 app = Flask(__name__)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page404.html'), 404
+
+@app.errorhandler(500)
+def error500(error):
+    return render_template("page500.html", erroHttp=error)
 
 app.register_blueprint(bp_funcionario)
 app.register_blueprint(bp_cliente)

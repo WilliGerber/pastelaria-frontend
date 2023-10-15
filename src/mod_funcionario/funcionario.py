6cc484987da63c81abe7fa6868d9a58b4463f9e1
WilliGerber <49000944+WilliGerber@users.noreply.github.com>
2023-10-15 19:27:23 -0300

    create funcionario list page

--- src/mod_funcionario/funcionario.py
+++ src/mod_funcionario/funcionario.py
@@ -3,5 +3,9 @@ from flask import Blueprint, render_template
 bp_funcionario = Blueprint('funcionario', __name__, url_prefix="/funcionario", template_folder='templates')
 
 @bp_funcionario.route('/')
-def formListaFuncionario():
-    return render_template('formListaFuncionario.html'), 200
+def listaFuncionario():
+    return render_template('listaFuncionario.html'), 200
+
+@bp_funcionario.route('/form')
+def formFuncionario():
+    return render_template('formFuncionario.html'), 200
